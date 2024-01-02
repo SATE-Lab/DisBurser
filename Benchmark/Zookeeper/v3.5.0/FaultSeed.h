e77725588c44f60a57058b76cb766e5cf9253c1d
zdc <zdc2022@outlook.com>
2024-01-02 15:55:56 +0800

    catch-up fix for revision of zk-2355

--- Benchmark/Zookeeper/v3.5.0/FaultSeed.h
+++ Benchmark/Zookeeper/v3.5.0/FaultSeed.h
@@ -1,3 +1,4 @@
 /* #define ZK_706 */
 /* #define ZK_1366 */
 /* #define ZK_2052 */
+/* #define ZK_2355 */
--- Benchmark/Zookeeper/v3.5.0/inject.c
+++ Benchmark/Zookeeper/v3.5.0/inject.c
@@ -73,4 +73,15 @@ int main()
 #endif
 
 
+#ifdef ZK_2355
+	system("cp ./buggy/Learner.java ./apache-zookeeper-3.5.3/src/java/main/org/apache/zookeeper/server/quorum/Learner.java");
+	system("cp ./buggy/QuorumPeer.java ./apache-zookeeper-3.5.3/src/java/main/org/apache/zookeeper/server/quorum/QuorumPeer.java");
+	system("cp ./buggy/QuorumPeerMain.java ./apache-zookeeper-3.5.3/src/java/main/org/apache/zookeeper/server/quorum/QuorumPeerMain.java");
+	printf("inject ZK_2355...\n");
+#else
+	system("cp ./fixed/Learner.java ./apache-zookeeper-3.5.3/src/java/main/org/apache/zookeeper/server/quorum/Learner.java");
+	system("cp ./fixed/QuorumPeer.java ./apache-zookeeper-3.5.3/src/java/main/org/apache/zookeeper/server/quorum/QuorumPeer.java");
+	system("cp ./fixed/QuorumPeerMain.java ./apache-zookeeper-3.5.3/src/java/main/org/apache/zookeeper/server/quorum/QuorumPeerMain.java");
+	printf("don't inject ZK_2355...\n");
+#endif
 }
